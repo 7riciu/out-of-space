b6ac7d13b1dd7f44bb1c281a18760a228c7da8d6
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2026-04-12 14:45:46 +0300

    Added a slime attack

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @onready var slime_area = get_tree().get_first_node_in_group("slime_area")
 @onready var player = get_tree().get_first_node_in_group("player")
+@onready var slime_splash_scene = preload("res://scenes/slime_splash.tscn")
+@onready var slime_splash_instance = null
 @onready var happy = false
 
 func _process(_delta: float) -> void:
@@ -10,6 +12,10 @@ func _process(_delta: float) -> void:
 			$AnimatedSprite2D.play("angry")
 			await get_tree().create_timer(2.0).timeout
 			$AnimatedSprite2D.play("attack")
+			if slime_splash_instance == null:
+				slime_splash_instance = slime_splash_scene.instantiate()
+				get_tree().current_scene.add_child(slime_splash_instance)
+				slime_splash_instance.position = self.position + Vector2(25, 0)
 		if happy:
 			$AnimatedSprite2D.play("idle")
 	else:
